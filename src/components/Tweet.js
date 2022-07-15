3f37a6bfa1597b83caa0677b87a2f87251d1ccc4
unknown <nmsmahavir267@gmail.com>
2022-07-15 17:29:07 +0530

    Heart color update

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
-import moment from 'moment';
-import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import Reply from './Reply';
-import ReplyModal from './ReplyModal';
-import { BsSuitHeart } from 'react-icons/bs';
+import axios from "axios";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Reply from "./Reply";
+import ReplyModal from "./ReplyModal";
+import { BsSuitHeart } from "react-icons/bs";
 const Tweet = ({ tweet, deleteTweet }) => {
   const nav = useNavigate();
   const [isEdit, setIsEdit] = useState(false);
@@ -14,20 +14,20 @@ const Tweet = ({ tweet, deleteTweet }) => {
   const [replies, setReplies] = useState([]);
   const [likes, setLikes] = useState(0);
   const location = useLocation();
-  const username = sessionStorage.getItem('username');
-  const token = sessionStorage.getItem('$myToken$');
+  const username = sessionStorage.getItem("username");
+  const token = sessionStorage.getItem("$myToken$");
 
   const onTextHandler = (e) => {
     setText(e.target.value);
     if (e.target.value.trim().length < 0) {
       setValidText({
         valid: false,
-        text: 'Please tweet something before post',
+        text: "Please tweet something before post",
       });
     } else if (e.target.value.trim().length > 144) {
       setValidText({
         valid: false,
-        text: 'Tweet must not exceed 145 characters',
+        text: "Tweet must not exceed 145 characters",
       });
     } else setValidText({ valid: true, text: null });
   };
@@ -49,12 +49,12 @@ const Tweet = ({ tweet, deleteTweet }) => {
         }
       )
       .then((response) => {
-        if (response.status === 200) alert('Tweet updated sucessfully');
+        if (response.status === 200) alert("Tweet updated sucessfully");
       })
       .catch((err) => {
         if (err.response.status === 403) {
-          alert('Login required to view this page, Please Login');
-          nav('/');
+          alert("Login required to view this page, Please Login");
+          nav("/");
         } else {
           if (err.response.data) {
             let message = err.response.data.message;
@@ -66,7 +66,7 @@ const Tweet = ({ tweet, deleteTweet }) => {
 
             alert(pretty);
           } else {
-            alert(err.message + ' Try again after some time');
+            alert(err.message + " Try again after some time");
           }
         }
       });
@@ -74,7 +74,7 @@ const Tweet = ({ tweet, deleteTweet }) => {
   };
 
   const onDelete = () => {
-    const confirm = window.confirm('Are you sure want to delete the tweet');
+    const confirm = window.confirm("Are you sure want to delete the tweet");
     if (confirm) {
       axios
         .delete(
@@ -85,14 +85,14 @@ const Tweet = ({ tweet, deleteTweet }) => {
         )
         .then((response) => {
           if (response.status === 200) {
-            alert('Tweet deleted sucessfully');
+            alert("Tweet deleted sucessfully");
             deleteTweet(response.data.data.id);
           }
         })
         .catch((err) => {
           if (err.response.status === 403) {
-            alert('Login required to view this page, Please Login');
-            nav('/');
+            alert("Login required to view this page, Please Login");
+            nav("/");
           } else {
             if (err.response.data) {
               let message = err.response.data.message;
@@ -104,7 +104,7 @@ const Tweet = ({ tweet, deleteTweet }) => {
 
               alert(pretty);
             } else {
-              alert(err.message + ' Try again after some time');
+              alert(err.message + " Try again after some time");
             }
           }
         });
@@ -129,15 +129,13 @@ const Tweet = ({ tweet, deleteTweet }) => {
       )
       .then((response) => {
         if (response.status === 200) {
-          console.log(response);
           setLikes(response.data.data.likes);
         }
       })
       .catch((err) => {
-        console.log(err);
         if (err.response.status === 403) {
-          alert('Login required to view this page, Please Login');
-          nav('/');
+          alert("Login required to view this page, Please Login");
+          nav("/");
         } else {
           if (err.response.data) {
             let message = err.response.data.message;
@@ -149,7 +147,7 @@ const Tweet = ({ tweet, deleteTweet }) => {
 
             alert(pretty);
           } else {
-            alert(err.message + ' Try again after some time');
+            alert(err.message + " Try again after some time");
           }
         }
       });
@@ -165,82 +163,88 @@ const Tweet = ({ tweet, deleteTweet }) => {
   }, [tweet.replies, tweet.likes]);
 
   return (
-    <div className='card fs-5 m-2' key={tweet.id}>
-      <div className='card-body'>
-        <p className='card-title'>
-          <span className='badge rounded-pill bg-success fs-4'>
+    <div className="card fs-5 m-2" key={tweet.id}>
+      <div className="card-body">
+        <p className="card-title">
+          <span className="badge rounded-pill bg-success fs-4">
             {tweet.username.charAt(0).toUpperCase()}
           </span>
-          <span className='m-2 fs-5'>{tweet.username}</span>
-          <span className='fs-6 text-muted'>
+          <span className="m-2 fs-5">{tweet.username}</span>
+          <span className="fs-6 text-muted">
             {moment(tweet.timestamp).fromNow()}
           </span>
         </p>
 
         {isEdit ? (
           <>
-            <div className='row'>
+            <div className="row">
               <textarea
-                type='text'
-                className='form-control'
-                id='InputUsername'
-                placeholder='Tweet Here'
-                name='text'
+                type="text"
+                className="form-control"
+                id="InputUsername"
+                placeholder="Tweet Here"
+                name="text"
                 value={text}
                 onChange={onTextHandler}
                 required
               />
             </div>
-            <div className='row m-2'>
+            <div className="row m-2">
               <button
-                className='btn btn-outline-success btn-sm col'
+                className="btn btn-outline-success btn-sm col"
                 onClick={updateTweet}
-                disabled={disabled}>
+                disabled={disabled}
+              >
                 Save
               </button>
               <button
-                className='btn btn-outline-success btn-sm col'
+                className="btn btn-outline-success btn-sm col"
                 onClick={() => {
                   setIsEdit(false);
-                }}>
+                }}
+              >
                 Cancel
               </button>
             </div>
           </>
         ) : (
           <>
-            <div className='card-text row'>
-              <span className='col-8 fs-5'>{text}</span>
+            <div className="card-text row">
+              <span className="col-8 fs-5">{text}</span>
               <button
-                className='btn btn-outline col-2 p-0'
-                onClick={updateLikes}>
-                <BsSuitHeart />
+                className="btn btn-outline col-2 p-0"
+                onClick={updateLikes}
+              >
+                <BsSuitHeart color={likes ? "rgb(224, 36, 94)" : ""} />
               </button>
-              <span className='col-2'>{likes}</span>
+              <span className="col-2">{likes}</span>
             </div>
-            <div className='row'>
+            <div className="row">
               <button
-                type='button'
-                className='btn btn-outline-info btn-sm m-2 col'
-                data-bs-toggle='modal'
-                data-bs-target='#staticBackdrop'>
+                type="button"
+                className="btn btn-outline-info btn-sm m-2 col"
+                data-bs-toggle="modal"
+                data-bs-target="#staticBackdrop"
+              >
                 Reply
               </button>
               <ReplyModal updateReplies={updateReplies} tweetId={tweet.id} />
-              {location.pathname === '/myTweets' && (
+              {location.pathname === "/myTweets" && (
                 <>
                   <button
-                    type='buttton'
-                    className='btn btn-outline-success btn-sm m-2 col'
+                    type="buttton"
+                    className="btn btn-outline-success btn-sm m-2 col"
                     onClick={() => {
                       setIsEdit(true);
-                    }}>
+                    }}
+                  >
                     Edit
                   </button>
                   <button
-                    type='button'
-                    className='btn btn-outline-danger btn-sm m-2 col'
-                    onClick={onDelete}>
+                    type="button"
+                    className="btn btn-outline-danger btn-sm m-2 col"
+                    onClick={onDelete}
+                  >
                     Delete
                   </button>
                 </>
@@ -250,7 +254,7 @@ const Tweet = ({ tweet, deleteTweet }) => {
         )}
       </div>
       {replies && replies.length > 0 && (
-        <div className='card-footer text-muted'>
+        <div className="card-footer text-muted">
           {replies.map((reply, index) => (
             <Reply reply={reply} key={index} />
           ))}
